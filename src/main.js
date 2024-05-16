3d9df179622edb98ae4e01ff44f0090f65d8f3e2
manolo0o <147361804+manolo0o@users.noreply.github.com>
2024-05-16 17:24:32 -0500

    feat: :construction: Working On the Html Structure.

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-// IMPORTING LIT ELEMENTS//
+// IMPORTING LIT ELEMENTS
 import { LitElement, css, html } from 'lit'
 class Myelement extends LitElement  {
     constructor(){
@@ -6,11 +6,30 @@ class Myelement extends LitElement  {
         this.shuffleButton = "/public/shuffleButton.svg";
     }
     static styles =css`
-
     `
+// HTML STRUCTURE
     render(){
         return html`
-         
+        <div class="wrapper">
+            <aside>
+                <header>
+                    <h1 class= "logo">CampusShop</h1>
+                </header>
+                <nav>
+                    <ul>
+                        <li><button class="button__Category">All Products</button></li>
+                        <li><button class="button__Category"> Coats</button></li>
+                        <li><button class="button__Category">Shirts</button></li>
+                        <li><button class="button__Category">Pants</button></li>
+                        <li><a class = "cart__Button"><box-icon type='solid' name='cart'></box-icon>Cart <span class="number">0</span></a></li>
+                    </ul>
+                </nav>
+            </aside>
+            
+            <main>
+                
+            </main>
+        </div>
     `
     } 
 }
