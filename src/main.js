7422830c660d7bd320f6fb67151813be9a607e59
manolo0o <147361804+manolo0o@users.noreply.github.com.>
2024-05-16 23:44:18 -0500

    feat: :construction: Working on the HTML and CSS

--- src/main.js
+++ src/main.js
@@ -6,8 +6,57 @@ class Myelement extends LitElement  {
         this.shuffleButton = "/public/shuffleButton.svg";
     }
     static styles =css`
+    .wrapper{
+        display:grid;
+        grid-template-columns:1fr 4fr;
+        background-color: var(--clr-main);
+    }
+
+    aside{
+        padding:2rem;
+        color:var(--clr-white);
+        position:sticky;
+        top:0;
+        height: 100vh;
+        display:flex;
+        flex-direction:column;
+        justify-content:space-between;
+    }
+    
+    .logo{
+        font-weight:400;
+        font-size:1.3rem;
+    }
+
+    .footer__text{
+        color:var(--clr-main-light);
+        font-size:.85rem;
+    }
+    
+    main{
+        background-color:var(--clr-white);
+        margin:1rem;
+        margin-left:0;
+        border-radius:2rem;
+        padding:3rem;
+    }
+
+    .principal__Title{
+        color:var(--clr-main);
+        margin-bottom: 2rem;
+    }
+
+    .product__container{
+        display:grid;
+        grid-template-columns:repeat(4, 1fr);
+        gap:1rem;
+    }
+
+    .product__Image{
+        max-width:100%;
+        border-radius:1rem;
+    }
     `
-// HTML STRUCTURE
     render(){
         return html`
         <div class="wrapper">
@@ -24,10 +73,63 @@ class Myelement extends LitElement  {
                         <li><a class = "cart__Button"><box-icon type='solid' name='cart'></box-icon>Cart <span class="number">0</span></a></li>
                     </ul>
                 </nav>
+                <footer>
+                    <p class="footer__text">© 2024 CampusShop</p>
+                </footer>
             </aside>
             
             <main>
-                
+                <h2 class="principal__Title">Todos los productos</h2>
+                <div class="product__container">
+                    <div class="products">
+                        
+                        <img class="product__Image" src="./public/abrigo1.webp" alt="">
+                        <div class="product__Details">
+                            
+                            <h3 class="product__Title">Abrigo 1</h3>
+                            
+                            <p class="product__Price">$1000</p>
+                            <button class="add__product">Agregar</button>
+                        
+                        </div>
+                        <div class="products">
+                        
+                        <img class="product__Image" src="./public/abrigo1.webp" alt="">
+                        <div class="product__Details">
+                            
+                            <h3 class="product__Title">Abrigo 2</h3>
+                            
+                            <p class="product__Price">$1000</p>
+                            
+                            <button class="add__product">Agregar</button>
+                        
+                        </div>
+                        <div class="products">
+                        
+                        <img class="product__Image" src="./public/abrigo1.webp" alt="">
+                        <div class="product__Details">
+                            
+                            <h3 class="product__Title">Abrigo 3</h3>
+                            
+                            <p class="product__Price">$1000</p>
+                            
+                            <button class="add__product">Agregar</button>
+                        
+                        </div>
+                        <div class="products">
+                        
+                        <img class="product__Image" src="./public/abrigo1.webp" alt="">
+                        <div class="product__Details">
+                            
+                            <h3 class="product__Title">Abrigo 4</h3>
+                            
+                            <p class="product__Price">$1000</p>
+                            
+                            <button class="add__product">Agregar</button>
+                        
+                        </div>
+                    </div> 
+                </div>
             </main>
         </div>
     `
