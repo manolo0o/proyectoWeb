574f0e61a61cb83b527949a4f699107c95815679
manolo0o <147361804+manolo0o@users.noreply.github.com.>
2024-05-25 16:55:10 -0500

    feat: :bug: port bug

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite'
 export default defineConfig({
         server: {
                 host:"172.16.101.146",
-                port:5592
+                port:5593
         }
 })
