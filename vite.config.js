82c11b4101e28cf41b1c0a980f2d8b4df7a852bc
manolo0o <147361804+manolo0o@users.noreply.github.com.>
2024-05-25 16:22:43 -0500

    fix: :bug: fixing problems with the port, to reload to the campus server

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
         server: {
-                host:"172.16.102.146",
+                host:"172.16.101.146",
                 port:5592
         }
 })
